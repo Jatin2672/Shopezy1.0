351459366992a8865d3d8355f4c9b0c6231e7d8a
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2021-10-28 15:31:41 +0530

    combining both page

---
+++ comb/Script.js
@@ -0,0 +1,28 @@
+const login_card = document.getElementById("login_card")
+const new_acc = document.getElementsByClassName("container-2")
+const reg_btn = document.getElementById("register")
+const already = document.getElementById("already")
+
+
+reg_btn.addEventListener("click", () => {
+    login_card.style.display = "none"
+    new_acc.style.display = "flex"
+    // Added basic animation keyframe
+    // login_card.animate([
+    //     { opacity: '1' },
+    //     { transform: 'translateX(0px)' },
+    //     { transform: 'translateX(60px)' },
+    // ], {
+    //     duration: 1000
+    // })
+
+    // new_acc.animate([
+    //     { opacity: '0' },
+    //     { transform: 'translateX(60px)' },
+    //     { transform: 'translateX(0px)' },
+    // ], {
+    //     duration: 500
+    // })
+
+
+})
