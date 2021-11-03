d9bdf9871dec8db6139c15e21153c51f7e6d1aa9
Divyanshu Kunwar <68821907+kUNWAR-DIVYANSHU@users.noreply.github.com>
2021-11-03 23:29:10 +0530

    sidebar

--- script/dashboardScreen.js
+++ script/dashboardScreen.js
@@ -1,3 +1,29 @@
+let expandMenuBtn ,tray , profilepic , menuExpanded = true
 window.addEventListener("DOMContentLoaded",() => {
-    
+
+   expandMenuBtn = document.getElementById("expandMenuBtn")
+   tray = document.getElementById("tray")
+   profilepic = document.getElementById("profilepic")
+
+    expandMenuBtn.addEventListener("click",() => {
+        collapseExpandMenu()
+    })
 })
+function collapseExpandMenu() {
+    allSpans = document.getElementById("tray").getElementsByTagName("span")
+    if(menuExpanded) {
+           tray.style.width = "5%"
+    for(let i = 0 ; i < allSpans.length ; i++) {
+        allSpans[i].style.display = "none"
+        profilepic.style.marginLeft = "10px"
+
+    }
+    }else{
+        tray.style.width = "15%"
+        for(let i = 0 ; i < allSpans.length ; i++) {
+            allSpans[i].style.display = "block"
+            profilepic.style.marginLeft = "0px"
+        }
+    }
+    menuExpanded = !menuExpanded
+}
