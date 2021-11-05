8c69a3858fd347d7083a5d7f3a8d5c2dcea7d439
Divyanshu Kunwar <68821907+kUNWAR-DIVYANSHU@users.noreply.github.com>
2021-11-06 00:36:54 +0530

    json and other added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // main.js
 
-const { ipcMain, app, BrowserWindow } = require('electron')
-const path = require('path')
+const { ipcMain, app, BrowserWindow } = require("electron");
+const path = require("path");
 
-let welcomeWindow, dashboardWindow
+let welcomeWindow, dashboardWindow;
 
 function createWelcomeWindow() {
   // Create the browser window.
@@ -14,11 +14,11 @@ function createWelcomeWindow() {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      preload: path.join(__dirname, 'script/welcome_screen.js')
-    }
-  })
-  welcomeWindow.loadFile('windows_html/welcome_screen.html')
-  welcomeWindow.maximize()
+      preload: path.join(__dirname, "script/welcome_screen.js"),
+    },
+  });
+  welcomeWindow.loadFile("windows_html/welcome_screen.html");
+  welcomeWindow.maximize();
 }
 function createdashboardWindow() {
   dashboardWindow = new BrowserWindow({
@@ -28,75 +28,127 @@ function createdashboardWindow() {
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
-      preload: path.join(__dirname, 'script/dashboardScreen.js')
-    }
-  })
+      preload: path.join(__dirname, "script/dashboardScreen.js"),
+    },
+  });
 
-  dashboardWindow.loadFile('windows_html/dashboardScreen.html')
-  dashboardWindow.maximize()
-
+  dashboardWindow.loadFile("windows_html/dashboardScreen.html");
+  dashboardWindow.maximize();
 }
 app.whenReady().then(() => {
-  createWelcomeWindow()
-  createServer()
-})
+  createWelcomeWindow();
+  createServer();
+});
 
-ipcMain.on('welcome:close', (event, arg) => {
-  welcomeWindow.close()
-})
-ipcMain.on('welcome:minimize', (event, arg) => {
-  welcomeWindow.minimize()
-})
-ipcMain.on('welcome:maximize', (event, arg) => {
+ipcMain.on("welcome:close", (event, arg) => {
+  welcomeWindow.close();
+});
+ipcMain.on("welcome:minimize", (event, arg) => {
+  welcomeWindow.minimize();
+});
+ipcMain.on("welcome:maximize", (event, arg) => {
   if (welcomeWindow.isMaximized()) {
-    welcomeWindow.unmaximize()
+    welcomeWindow.unmaximize();
   } else {
-    welcomeWindow.maximize()
+    welcomeWindow.maximize();
   }
-})
+});
 
-ipcMain.on('dashboard:close', () => {
-  dashboardWindow.close()
-})
-ipcMain.on('dashboard:minimize', () => {
-  dashboardWindow.minimize()
-})
-ipcMain.on('dashboard:maximize', () => {
+ipcMain.on("dashboard:close", () => {
+  dashboardWindow.close();
+});
+ipcMain.on("dashboard:minimize", () => {
+  dashboardWindow.minimize();
+});
+ipcMain.on("dashboard:maximize", () => {
   if (dashboardWindow.isMaximized()) {
-    dashboardWindow.unmaximize()
+    dashboardWindow.unmaximize();
   } else {
-    dashboardWindow.maximize()
+    dashboardWindow.maximize();
   }
-})
-ipcMain.on('go_to_dashboard', (event, arg) => {
-  createdashboardWindow()
-  welcomeWindow.close()
-})
+});
+ipcMain.on("go_to_dashboard", (event, arg) => {
+  createdashboardWindow();
+  welcomeWindow.close();
+});
 
 // -------------------------------RESTRICTED AREA---------------------------------------------//
-const http = require('http')
-const host = 'localhost'
-const port = 8000
+const http = require("http");
+const host = "localhost";
+const port = 8000;
+
+// import sqllite3
+const sqlite3 = require('sqlite3').verbose()
+
+
+// import ip from os module
+const { networkInterfaces } = require("os");
+
+function logIP() {
+  const getLocalExternalIP = () =>
+    []
+      .concat(...Object.values(networkInterfaces()))
+      .find((details) => details.family === "IPv4" && !details.internal)
+      .address;
+  console.log(getLocalExternalIP() + ":8000/connected");
+}
 
 function createServer() {
-
+  logIP();
   const requestListener = function (req, res) {
-    switch (req.url) {
+    const urls = req.url;
+    let urls_splitted, params;
+
+    // check if url contains ?
+    if (urls.includes("?")) {
+      // split url and get the first part
+       urls_splitted = urls.split("?")[0];
+      // parameter part
+       params = urls.split("?")[1];
+    }
+
+    switch (urls_splitted) {
       case "/connect":
-        res.writeHead(200)
-        res.end("connected Successfully")
-        break
+        res.writeHead(200);
+        res.end("connected Successfully");
+        console.log(params);
+        break;
+      case "/addItem":
+        res.writeHead(200);
+        res.end("addItem Successfully" + params);
+        break;
+      case "/availabilityItem":
+        res.writeHead(200);
+        res.end("10 items available");
+        break;
 
       default:
-        res.writeHead(404)
-        res.end("Not Found!")
-        break
+        res.writeHead(404);
+        res.end("Not Found!");
+        break;
     }
-  }
+  };
 
-  const server = http.createServer(requestListener)
+  const server = http.createServer(requestListener);
   server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`)
-  })
+    console.log(`Server is running on http://${host}:${port}`);
+  });
 
 }
+  // import fs
+  const fs = require("fs");
+
+  // open json file and write data to it
+  data = {
+    usernname: "Seller1",
+    email: "jjjgmail.coom",
+    phone: "1234567890",
+    password: "1234567890",
+    bussinessCategory: "Electronics",
+    bussinessName: "jakffibgi",
+    sellerName: "bvfvf",
+  }
+  fs.writeFile("settings/sellerinfo.json" , JSON.stringify(data) , (err) => {   
+    // write data to file
+    if (err) throw err;
+  });
