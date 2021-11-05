d86545a60de71ca6dd678466ae9bf88ff640b4f1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2021-11-05 22:01:04 +0530

    server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path')
 // import ipc main from electron
 
 let welcomeWindow, dashboardWindow;
-function createWelcomeWindow () {
+function createWelcomeWindow() {
   // Create the browser window.
   welcomeWindow = new BrowserWindow({
     width: 800,
@@ -48,20 +48,10 @@ function createdashboardWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWelcomeWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWelcomeWindow()
-  })
+  createServer()
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
@@ -76,26 +66,56 @@ ipcMain.on('welcome:minimize', (event, arg) => {
 })
 
 ipcMain.on('welcome:maximize', (event, arg) => {
-  if(welcomeWindow.isMaximized()) {
+  if (welcomeWindow.isMaximized()) {
     welcomeWindow.unmaximize()
   } else {
-  welcomeWindow.maximize()
+    welcomeWindow.maximize()
   }
 })
-ipcMain.on('go_to_dashboard', (event,arg) => {
+ipcMain.on('go_to_dashboard', (event, arg) => {
   createdashboardWindow()
   welcomeWindow.close()
 })
-ipcMain.on('dashboard:close',()=> {
+ipcMain.on('dashboard:close', () => {
   dashboardWindow.close()
 })
-ipcMain.on('dashboard:minimize',()=> {
+ipcMain.on('dashboard:minimize', () => {
   dashboardWindow.minimize()
 })
-ipcMain.on('dashboard:maximize',()=> {
-  if(dashboardWindow.isMaximized()) {
+ipcMain.on('dashboard:maximize', () => {
+  if (dashboardWindow.isMaximized()) {
     dashboardWindow.unmaximize()
   } else {
-  dashboardWindow.maximize()
+    dashboardWindow.maximize()
   }
 })
+// const ip = require('ip');
+// console.log(ip.address());
+const http = require('http');
+const host = 'localhost';
+const port = 8000;
+
+function createServer() {
+  const requestListener = function (req, res) {
+    switch (req.url) {
+      case "/connect":
+        res.writeHead(200)
+        res.end("connected Successfully")
+        break;
+    
+      default:
+        res.writeHead(404)
+        res.end("Not Found!")
+        break;
+    }
+    
+  }
+  const server = http.createServer(requestListener)
+
+  server.listen(port, host, () => {
+
+    console.log(`Server is running on http://${host}:${port}`)
+
+  })
+
+}
